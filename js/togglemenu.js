3dad855171db948b2214da93dfc69998f22e88b3
Moaning Malone <philippbuehler@protonmail.com>
2016-05-24 18:57:31 +0200

    reduced menu button size added images main content opaque on click

--- js/togglemenu.js
+++ js/togglemenu.js
@@ -1,11 +1,10 @@
 $(document).ready(function()
 {
-  // Side Menu Trigger Function
     $("button.toggle-nav").click(function(){
+    // Side Menu Trigger Function
         $("#sidenav").toggle();
-    });
+
   // Swap Hamburger for X
-  $("button.toggle-nav").on("click", function() {
     var el = $(this);
     if (el.text() == el.data("text-swap")) {
       el.text(el.data("text-original"));
@@ -13,5 +12,10 @@ $(document).ready(function()
       el.data("text-original", el.text());
       el.text(el.data("text-swap"));
     }
+    // Make Main Content opaque
+    $("#main").toggleClass("opaque");
+
+
   });
+  // end button toggle
 });
