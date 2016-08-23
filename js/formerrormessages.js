6bf3b0142a110b30a890ffc603be35adbce2127f
Moaning Malone <philippbuehler@protonmail.com>
2016-08-23 21:08:26 +0200

    mobilenav more line height

---
+++ js/formerrormessages.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+  function myFunction() {
+      var inpObj = document.getElementById("id1");
+      if (inpObj.checkValidity() == false) {
+          document.getElementById("demo").innerHTML = inpObj.validationMessage;
+      }
+  }
+
+});
