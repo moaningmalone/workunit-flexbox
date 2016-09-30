e0650849b634a941af86049798a50ec90773f258
Moaning Malone <philippbuehler@protonmail.com>
2016-09-30 16:51:07 +0200

    various

---
+++ js/togglemenu-footer-160930.js
@@ -0,0 +1,21 @@
+$(document).ready(function()
+{
+    $("button.toggle-nav").click(function(){
+    // Side Menu Trigger Function
+        $("#main-nav").toggle();
+        $("#main-nav").prependTo("#mobilenav");
+        $("#footernav").toggle();
+        $("#footernav").appendTo("#mobilenav");
+
+  // Swap Hamburger for X
+    var el = $(this);
+    if (el.text() == el.data("text-swap")) {
+      el.text(el.data("text-original"));
+    } else {
+      el.data("text-original", el.text());
+      el.text(el.data("text-swap"));
+    }
+
+  });
+  // end button toggle
+});
